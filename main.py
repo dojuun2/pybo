3df4289653c7a40df9bb4b9fa594a7b0d19f092b
DoJun Park <99643097+dojuun2@users.noreply.github.com>
2023-10-10 10:38:17 +0900

    feature: Fastapi와 Svelte 연결
    CORS 예외 처리

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware    # CORS 예외 처리 관련 라이브러리
 
 app = FastAPI()
+
+# CORS 예외 처리
+origins = [
+    "http://localhost:5173"     # 또는 "http://127.0.0.1:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/test")
