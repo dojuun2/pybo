159af8def8c8e0dcb3ad103b40f43944f2c35429
DoJun Park <99643097+dojuun2@users.noreply.github.com>
2023-11-10 13:20:52 +0900

    Refactor: 카테고리 이동 시, 페이지 번호 초기화

--- frontend/src/components/SideBar.svelte
+++ frontend/src/components/SideBar.svelte
@@ -1,19 +1,19 @@
 <script>
     import { link } from "svelte-spa-router";
-    import { category } from "../lib/store"
+    import { page, keyword, category, board_page } from "../lib/store"
 </script>
 
 <!-- 사이드바 -->
 <div class="d-flex flex-column flex-shrink-0 p-3 bg-white my-3" style="width: 280px;">
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
-        <a use:link href="/" class="nav-link {$category === "home" && 'active'}" aria-current="page" on:click={() => {$category="home"}}>
+        <a use:link href="/" class="nav-link {$category === "home" && 'active'}" aria-current="page" on:click={() => {$category="home", $page = 0, $keyword = ""}}>
             <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
             질문과 답변
         </a>
         </li>
         <li>
-        <a use:link href="/board" class="nav-link {$category === "board" && 'active'}" aria-current="page" on:click={() => {$category="board"}} >
+        <a use:link href="/board" class="nav-link {$category === "board" && 'active'}" aria-current="page" on:click={() => {$category="board", $board_page = 0}} >
             <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
             자유게시판
         </a>
